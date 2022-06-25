3a7b5398716240ace7c11137a5852aae4739c2f2
vivzhong <vivzhong@mit.edu>
2022-06-25 18:49:32 -0400

    github

--- weibo_followers.py
+++ weibo_followers.py
@@ -2,6 +2,52 @@ import requests
 import random
 import json
 from datetime import date
+from github import Github
+
+from config import get_github_access_token
+
+REPO_NAME = 'xz-weibo-followers'
+
+
+github = Github(get_github_access_token())
+
+def get_repos():
+    for repo in github.get_user().get_repos():
+        print(repo.name)
+
+
+def get_file(filename):
+    repository = github.get_user().get_repo(REPO_NAME)
+    file = repository.get_contents(filename)
+
+    print(file.url)
+
+    return file
+
+def put_file(filename, content):
+
+    repository = github.get_user().get_repo(REPO_NAME)
+
+    if check_file_exist(filename):
+        print(f'update_file {filename}')
+        f = get_file(filename)
+        f = repository.update_file(filename, "update_file via PyGithub", content, f.sha)
+    else:
+        print(f'create_file {filename}')
+        f = repository.create_file(filename, "create_file via PyGithub", content)
+
+def check_file_exist(filename):
+    try:
+        file = get_file(filename)
+    except:
+        return False
+
+    return True
+
+def get_user(name):
+    user = github.get_user(name)
+    return user
+
 
 actual_url = "https://m.weibo.cn/api/container/getIndex?type=uid&value="+str(1792951112)
 res = requests.get(actual_url).text
@@ -19,8 +65,11 @@ except Exception:
 today = date.today()
 day = today.strftime("%Y-%m-%d")
 
-g = open("xz_weibo_followers.txt", "a")
-g.write(day+","+followers+"\n")
-g.close()
+
+with open("xz_weibo_followers.txt", "a") as g:
+    g.write(day+","+followers+"\n")
+    content = g.read()
+
+put_file("xz_weibo_followers.txt", content)
 
 print(day)
